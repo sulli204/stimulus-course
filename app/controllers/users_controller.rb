211939d900803c4521f570ebeb9a0170b4eb545d
Patrick Sullivan <patrick@tanda.co>
2022-05-05 16:04:40 -0500

    Auth on signin works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    session[:used_id] = @user.id
+    session[:user_id] = @user.id
     redirect_to welcome_path
   end
 
